5154a86aa1173d304e07027374ef53aebf184a39
nirgivon <nirgivon@gmail.com>
2023-06-13 08:07:57 +0300

    add filter to params

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,50 @@ void main() {
   runApp(MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   MainApp({super.key});
 
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
   final List<Sensor> sensors = [];
+
   final DataModel dataModel = DataModel();
+  final _paramsFilterControler = TextEditingController();
+
+  List<Parameter> _filteredParameters = [];
+
+  @override
+  void initState() {
+    _paramsFilterControler.addListener(paramsFilterTextChanged);
+    _filteredParameters = dataModel.parameters;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _paramsFilterControler.dispose();
+    super.dispose();
+  }
+
+  void paramsFilterTextChanged() {
+    // print(_paramsFilterControler.text);
+
+    List<Parameter> res = [];
+    if (_paramsFilterControler.text.isEmpty) {
+      res = dataModel.parameters;
+    } else {
+      res = dataModel.parameters
+          .where((param) =>
+              param.name.toLowerCase().contains(_paramsFilterControler.text))
+          .toList();
+    }
+    setState(() {
+      _filteredParameters = res;
+    });
+  }
 
   void openFileDialog() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles(
@@ -73,6 +112,7 @@ class MainApp extends StatelessWidget {
         for (final sensor in sensors) {
           logger.d(sensor.parameterNames);
         }
+        setState(() {});
       }
     }
   }
@@ -81,9 +121,45 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: TextButton(
-              onPressed: openFileDialog, child: const Text('Click Me')),
+        body: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: TextField(
+                  controller: _paramsFilterControler,
+                  decoration: const InputDecoration(label: Text('Filter:')),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: ListView.builder(
+                itemCount: _filteredParameters.length,
+                itemBuilder: (ctx, index) => Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(_filteredParameters[index].name),
+                    ),
+                    const Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(_filteredParameters[index].value.toString()),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: Center(
+                child: TextButton(
+                    onPressed: openFileDialog, child: const Text('Click Me')),
+              ),
+            ),
+          ],
         ),
       ),
     );
